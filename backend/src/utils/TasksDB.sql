128d60f06642e2fc70df48d9a501a1e93c6cdf5e
lkotlarenko <lkotlarenko@protonmail.com>
2022-07-04 20:12:16 -0300

    chore(backend): 🔨 new script 'db:restore'
    add:
     script to restore default database (run with npm run db:restore).

---
+++ backend/src/utils/TasksDB.sql
@@ -0,0 +1,46 @@
+DROP SCHEMA IF EXISTS TasksDB;
+CREATE SCHEMA IF NOT EXISTS TasksDB;
+
+CREATE TABLE TasksDB.StatusTypes  (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  type TEXT NOT NULL
+);
+
+CREATE TABLE TasksDB.Tasks (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  task TEXT NOT NULL,
+  taskStatus INTEGER NOT NULL,
+  FOREIGN KEY (taskStatus) REFERENCES TasksDB.StatusTypes (id),
+  createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+INSERT INTO
+  TasksDB.StatusTypes (type)
+VALUES
+  ("done");
+
+INSERT INTO
+  TasksDB.StatusTypes (type)
+VALUES
+  ("ongoing");
+  
+INSERT INTO
+  TasksDB.StatusTypes (type)
+VALUES
+  ("canceled");
+
+INSERT INTO
+  TasksDB.Tasks (task, taskStatus)
+VALUES
+  ("eat", 1);
+
+INSERT INTO
+  TasksDB.Tasks (task, taskStatus)
+VALUES
+  ("code", 2);
+
+INSERT INTO
+  TasksDB.Tasks (task, taskStatus)
+VALUES
+  ("test db", 3);
---
+++ backend/src/utils/defaultDb.ts
@@ -0,0 +1,3 @@
+import { defaultDatabase } from './restoreDB';
+
+defaultDatabase();
