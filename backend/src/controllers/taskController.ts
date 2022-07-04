58a3809ca724c4a22c42d590b7716be59af3927d
lkotlarenko <lkotlarenko@protonmail.com>
2022-07-04 18:31:06 -0300

    feat(backend): ✨ new controller 'newTask'

--- backend/src/controllers/taskController.ts
+++ backend/src/controllers/taskController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes as httpCode } from 'http-status-codes';
+import ITask from '../interfaces/ITask';
 import taskService from '../services/taskService';
 
 const getAll = async (_req: Request, res: Response, next: NextFunction) => {
@@ -12,6 +13,18 @@ const getAll = async (_req: Request, res: Response, next: NextFunction) => {
   }
 };
 
+const newTask = async (req: Request, res: Response, next: NextFunction) => {
+  const { task, taskStatus }: ITask = req.body;
+  const taskData = { task, taskStatus };
+  try {
+    const createdTask = await taskService.newTask(taskData);
+    return res.status(httpCode.CREATED).json(createdTask);
+  } catch (err) {
+    next(err);
+  }
+};
+
 export default {
   getAll,
+  newTask,
 };
