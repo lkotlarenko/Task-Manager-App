eca5829c2f2abc2caa5efa5266a9cfcb8b5c78f4
lkotlarenko <lkotlarenko@protonmail.com>
2022-07-04 18:39:09 -0300

    feat(backend): ✨ new 'POST' endpoint '/create'
    add:
     new route '/create' to create a new task on database (require 'task'[string]' & 'taskStatus[number]' on body).

--- backend/src/router/index.ts
+++ backend/src/router/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import taskController from '../controllers/taskController';
+import taskValidator from '../middlewares/taskMiddleware';
 
 const router = Router();
 
 router.get('/tasks', taskController.getAll);
+router.post('/create', taskValidator, taskController.newTask)
 
 export default router;
