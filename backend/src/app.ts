df0cc2fc68bdfcb7f461312a3fbf5652bfd2a1a9
lkotlarenko <lkotlarenko@protonmail.com>
2022-07-05 11:46:58 -0300

    feat(backend): 🐛 add cors to backend API

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,16 @@
 import express from 'express';
 import errorMiddleware from './middlewares/errorMiddleware';
 import router from './router';
+import cors from 'cors';
 
 const app = express();
 
+const corsOptions ={
+   origin:'*',
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(router);
 app.use(errorMiddleware);
