f3532817aaccbd9a84ffd76c47c0e9da3e0356f5
lkotlarenko <lkotlarenko@protonmail.com>
2022-07-05 14:08:59 -0300

    feat(frontend): 🚧 update provider
    add:
     new states to provider.

--- frontend/src/context/ProviderApp.js
+++ frontend/src/context/ProviderApp.js
@@ -5,8 +5,12 @@ import { getAll } from '../api/taskAPI';
 
 function ProviderApp({ children }) {
   const [tasks, setTasks] = useState([]);
-  const [isLoading, setLoading] = useState(true)
+  const [isLoading, setLoading] = useState(true);
   const [search, setSearch] = useState('');
+  const [newTaskValue, setNewTaskValue] = useState('');
+  const [newTaskStatus, setNewTaskStatus] = useState(1);
+  const [needRefresh, setNeedRefresh] = useState(false);
+
   const context = {
     tasks,
     setTasks,
@@ -14,20 +18,28 @@ function ProviderApp({ children }) {
     setLoading,
     search,
     setSearch,
+    newTaskValue,
+    setNewTaskValue,
+    newTaskStatus,
+    setNewTaskStatus,
+    needRefresh,
+    setNeedRefresh,
+  };
+
+  const fetchTasks = async () => {
+    await getAll(setTasks, setLoading);
+    setNeedRefresh(false);
   };
 
   useEffect(() => {
-    const fetchTasks = async () => {
-      await getAll(setTasks, setLoading);
-    };
     fetchTasks();
   }, [setLoading, setTasks]);
-  
-  return (
-    <ContextApp.Provider value={ context }>
-      { children }
-    </ContextApp.Provider>
-  );
+
+  useEffect(() => {
+    fetchTasks();
+  }, [needRefresh]);
+
+  return <ContextApp.Provider value={context}>{children}</ContextApp.Provider>;
 }
 
 ProviderApp.propTypes = {
@@ -35,4 +47,3 @@ ProviderApp.propTypes = {
 }.isRequired;
 
 export default ProviderApp;
-
