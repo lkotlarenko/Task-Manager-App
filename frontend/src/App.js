27faf817297b81b0f5282605e630a7d7ae5602fa
lkotlarenko <lkotlarenko@protonmail.com>
2022-07-05 14:14:24 -0300

    feat(frontend): ✨ add 'TasksManager' component to 'App'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TasksTable from './components/TasksTable';
+import TasksManager from './components/TasksManager';
 
 const App = () => {
   return (
     <>
       <header>Tasks List</header>
       <main>
+        <TasksManager />
         <TasksTable />
       </main>
     </>
