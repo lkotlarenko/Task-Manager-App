26511722d7907ea468abe4329b503a3677b3f746
lkotlarenko <lkotlarenko@protonmail.com>
2022-07-05 03:21:12 -0300

    feat(backend): ✨ new service 'removeTask'

--- backend/src/services/taskService.ts
+++ backend/src/services/taskService.ts
@@ -9,15 +9,21 @@ const getAll = async (): Promise<ITask[]> => {
 const newTask = async (data: ITask): Promise<ITask> => {
   const addTask = await taskModel.newTask(data);
   return addTask;
-}
+};
 
 const editTask = async (data: ITask): Promise<ITask> => {
   const updatedTask = await taskModel.editTask(data);
   return updatedTask;
-}
+};
+
+const removeTask = async (id: string | number): Promise<void> => {
+  await taskModel.checkById(id);
+  await taskModel.removeTask(id);
+};
 
 export default {
   getAll,
   newTask,
   editTask,
+  removeTask,
 };
