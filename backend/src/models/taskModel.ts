d0678b644abd72c8ffcd788064d6b2e62f1cfd70
lkotlarenko <lkotlarenko@protonmail.com>
2022-07-05 03:20:34 -0300

    feat(backend): ✨ new models 'removeTask' & 'checkById'

--- backend/src/models/taskModel.ts
+++ backend/src/models/taskModel.ts
@@ -1,6 +1,7 @@
 import { ResultSetHeader } from 'mysql2';
 import ITask from '../interfaces/ITask';
 import connection from './connection';
+import { StatusCodes as httpCode } from 'http-status-codes';
 
 const getAll = async (): Promise<ITask[]> => {
   const query = `SELECT tas.id, tas.task, sta.type AS taskStatus, tas.createdAt, tas.updatedAt
@@ -14,22 +15,41 @@ const getAll = async (): Promise<ITask[]> => {
 const newTask = async (data: ITask): Promise<ITask> => {
   const { task, taskStatus } = data;
   const query = 'INSERT INTO TasksDB.Tasks (task, taskStatus) VALUES(?, ?)';
-  const [createdTask] = await connection.execute<ResultSetHeader>(query, [task, taskStatus]);
+  const [createdTask] = await connection.execute<ResultSetHeader>(query, [
+    task,
+    taskStatus,
+  ]);
   const { insertId: id } = createdTask;
   const newTask = { id, task, taskStatus };
   return newTask;
-}
+};
 
 const editTask = async (data: ITask): Promise<ITask> => {
   const { id, task, taskStatus } = data;
-  const query = 'UPDATE TasksDB.Tasks SET task = ?, taskStatus = ? WHERE (id = ?)';
+  const query =
+    'UPDATE TasksDB.Tasks SET task = ?, taskStatus = ? WHERE (id = ?)';
   await connection.execute<ResultSetHeader>(query, [task, taskStatus, id]);
   const newTask = { id, task, taskStatus };
   return newTask;
-}
+};
+
+const removeTask = async (id: string | number): Promise<void> => {
+  const query = 'DELETE FROM TasksDB.Tasks WHERE id = ?';
+  await connection.execute<ResultSetHeader>(query, [id]);
+};
+
+const checkById = async (id: string | number): Promise<void> => {
+  const query = 'SELECT task FROM TasksDB.Tasks WHERE id = ?';
+  const [check]: any[] = await connection.execute(query, [id]);
+  if (check.length === 0) {
+    throw { status: httpCode.BAD_REQUEST, message: 'invalid id' };
+  }
+};
 
 export default {
   getAll,
   newTask,
   editTask,
+  removeTask,
+  checkById,
 };
